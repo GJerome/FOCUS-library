f309c633f0dbf784d889791ced93fc2974016001
GJerome <jejegautier@gmail.com>
2023-09-14 14:04:39 +0200

    MISC

--- Exp_ChopperShutterPhaseOptimisation.py
+++ Exp_ChopperShutterPhaseOptimisation.py
@@ -6,7 +6,7 @@ import numpy as np
 
 import FileControl 
 import time
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 #############################
 # Global parameter
@@ -34,7 +34,7 @@ InstrumentsPara['Lock-in-amplifier']=LockInDevice.parameterDict
 # Initialisation of laser
 #############################
 
-Laser= las.LaserControl('COM6',2)
+Laser= las.LaserControl('COM8',2)
 
 InstrumentsPara['Laser']=Laser.parameterDict
 
@@ -42,7 +42,7 @@ InstrumentsPara['Laser']=Laser.parameterDict
 # Initialisation of Chopper
 #############################
 
-Chopper1= chop.OpticalChopper('COM14')
+Chopper1= chop.OpticalChopper('COM11')
 Chopper1.SetInternalFrequency(0)
 Chopper1.SetMotorStatus('ON')
 Chopper1.WaitForLock(10)
@@ -60,7 +60,7 @@ DirectoryPath=FileControl.PrepareDirectory(GeneralPara,InstrumentsPara)
 #############################
 print('Begin acquisition')
 #angle=range(0,360,0.1)
-angle=np.arange(95,300,1)
+angle=np.arange(0,3600,5)
 
 IntensityData=np.zeros(len(angle))
 IntensityData2=np.zeros(len(angle))
@@ -73,7 +73,7 @@ print("Everything is ready")
 Laser.StatusShutterTunable(1)
 for k in  temp_iterator:
     Laser.StatusShutterTunable(1)
-    LockInDevice.AutorangeSource()
+    #LockInDevice.AutorangeSource()
     Chopper1.SetPhase(np.round(k, decimals=2))
     Chopper1.WaitForLock(10)
     time.sleep(2)
@@ -87,7 +87,7 @@ for k in  temp_iterator:
 
     data_Source2_interp=np.interp(t1,t2,data_Source2)
     t1_scaled=(t1-t1[1])/LockInDevice.Timebase
-    plt.scatter(t1_scaled[1:],data_Source1[1:])
+    #plt.scatter(t1_scaled[1:],data_Source1[1:])
     print(np.round(k, decimals=2))
     IntensityData[temp_iterator.index]=np.mean(data_Source1)
     ErrorBar[temp_iterator.index]=np.std(data_Source1)
@@ -97,6 +97,6 @@ for k in  temp_iterator:
 ExportData=np.array(np.transpose([angle,IntensityData,ErrorBar,IntensityData2,ErrorBar2]))
 FileControl.ExportFileChopperOptimisation(DirectoryPath,FileNameData+'loop{}'.format(str(k)),ExportData)
 Laser.StatusShutterTunable(0)
-plt.show()
+#plt.show()
 
       
