dba3ef4c904fa2c3950c6ae1c9951a2d31498ab6
GJerome <jejegautier@gmail.com>
2025-02-25 12:18:27 +0100

    Fix: tidying up and fixing coment

--- ControlFilterWheel.py
+++ ControlFilterWheel.py
@@ -2,8 +2,9 @@ try:
     from pylablib.devices import Thorlabs
 except Exception as ex:
     print(ex)
-    
+
 import time
+
 
 class FilterWheel:
     """
@@ -13,10 +14,11 @@ class FilterWheel:
         conn: serial connection parameters (usually port or a tuple containing port and baudrate)
         respect_bound(bool): if ``True``, avoid crossing the boundary between the first and the last position in the wheel
     """
-  
+
     def __init__(self, conn, respect_bound=True):
         self.device = Thorlabs.serial.FW(conn, respect_bound)
         self.respect_bound = respect_bound
+        self.parameterDict = {'Com port': conn}
 
     def get_position(self):
         """Get the wheel position (starting from 1)"""
@@ -24,11 +26,11 @@ class FilterWheel:
 
     def set_position(self, pos):
         """Set the wheel position (starting from 1)"""
-        if self.respect_bound: # check if the wheel could go through zero; if so, manually go around instead
-            cur_pos=self.get_position()
-            if abs(pos-cur_pos)>=self.device.pcount//2: # could switch by going through zero
-                medp1=(2*cur_pos+pos)//3
-                medp2=(cur_pos+2*pos)//3
+        if self.respect_bound:  # check if the wheel could go through zero; if so, manually go around instead
+            cur_pos = self.get_position()
+            if abs(pos-cur_pos) >= self.device.pcount//2:  # could switch by going through zero
+                medp1 = (2*cur_pos+pos)//3
+                medp2 = (cur_pos+2*pos)//3
                 self.device.set_position(int(medp1))
                 self.device.set_position(int(medp2))
                 self.device.set_position(int(pos))
@@ -50,7 +52,7 @@ class FilterWheel:
         """Get the motion speed mode (``"low"`` or ``"high"``)"""
         return self.device.get_speed_mode()
 
-    def set_speed_mode(self, speed_mode = "high"):
+    def set_speed_mode(self, speed_mode="high"):
         """Set the motion speed mode (``"low"`` or ``"high"``)"""
         return self.device.set_speed_mode(speed_mode)
 
@@ -58,7 +60,7 @@ class FilterWheel:
         """Get the trigger mode (``"in"`` to input external trigger, ``"out"`` to output trigger)"""
         return self.device.get_trigger_mode()
 
-    def set_trigger_mode(self, trigger_mode = "out"):
+    def set_trigger_mode(self, trigger_mode="out"):
         """Set the trigger mode (``"in"`` to input external trigger, ``"out"`` to output trigger)"""
         return self.device.set_trigger_mode(trigger_mode)
 
@@ -66,9 +68,10 @@ class FilterWheel:
         """Get the sensor mode (``"off"`` to turn off when idle to eliminate stray light, ``"on"`` to remain on)"""
         return self.device.get_sensor_mode()
 
-    def set_sensor_mode(self, sensor_mode = "off"):
+    def set_sensor_mode(self, sensor_mode="off"):
         """Set the sensor mode (``"off"`` to turn off when idle to eliminate stray light, ``"on"`` to remain on)"""
         return self.device.set_sensor_mode(sensor_mode)
+
 
 if __name__ == "__main__":
 
@@ -76,7 +79,7 @@ if __name__ == "__main__":
   #  FW.get_position()
     FW.set_speed_mode()
     FW.set_position(1)
-    t0=time.time()
+    t0 = time.time()
     FW.set_position(2)
     print(time.time()-t0)
     print(FW.get_position())
