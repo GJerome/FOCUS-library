9e52eb2b3ea7f26d25fac3619fd54f344cf1d00d
GJerome <jejegautier@gmail.com>
2025-10-03 11:34:44 +0200

    Fix: reup file

--- PowerTimeExposureCycle/ML_Tools.py
+++ PowerTimeExposureCycle/ML_Tools.py
@@ -11,13 +11,14 @@ import random
 
 import ControlPiezoStage as Transla
 import ControlLaser as laser
+import ControlPulsePicker as picker
 
 import ControlPiezoStage as Transla
 import ControlConex as RoughTransla
 
 import FileControl
 from scipy.optimize import curve_fit
-
+from scipy.spatial import Delaunay
 
 #############################
 # Position generation
@@ -28,9 +29,50 @@ def DistanceTwoPoint(pointA, pointB):
 
 def DistanceArray(pointA, pointsB):
     return np.sqrt(np.sum((pointA - pointsB)**2, axis=1))
+def is_inside_convex_hull(x, y, points_2d):
+    """Check if a point is inside the convex hull using Delaunay triangulation."""
+    try:
+        tri = Delaunay(points_2d)
+        return tri.find_simplex(np.array([[x, y]])) >= 0
+    except:
+        return False
+
+def estimate_z(x, y, df, a, b, c):
+    
+    points_2d = df[['x', 'y']].to_numpy(dtype=float)
+    #return a * x + b * y + c
+
+    if not is_inside_convex_hull(x, y, points_2d):
+        print('Not inside hull: x={} y={}'.format(x,y))
+        return a * x + b * y + c
+
+    # Find 3 closest points in 2D
+    diff = points_2d - np.array([x, y])
+    distances = np.sqrt(np.sum(diff**2, axis=1,dtype=float))
+    closest_indices = np.argsort(distances)[:3]
+    pts = df.iloc[closest_indices]
+
+    # Get 3D coordinates
+    p1, p2, p3 = pts[['x', 'y', 'zFocus']].to_numpy(dtype=float)
+    
+    # Compute normal vector to the plane
+    v1 = p2 - p1
+    v2 = p3 - p1
+    print(v1)
+    normal = np.cross(v1, v2)
+    
+    # Handle degenerate case where points are collinear or near-collinear
+    if np.isclose(normal[2], 0):
+        return a * x + b * y + c
+
+    # Use point-normal form of the plane to solve for z
+    x0, y0, z0 = p1
+    nx, ny, nz = normal
+    z = z0 - (nx * (x - x0) + ny * (y - y0)) / nz
+    return z
 
 def Generate_RandomPositionRoughFine(generations_budget,Nb_Points_Generation,Nb_points_Piezo,
-                                     Coeff,BeamRadius=30,
+                                     Coeff,PointsCalib,BeamRadius=30,
                                      startX_rough=10,startY_rough=10,SpacingBetweenRough=0.150):
     GeneralPosition=pd.DataFrame(columns=['x_rough','y_rough','x_piezo','y_piezo','z_piezo','Cluster','Generation'])
 
@@ -47,6 +89,7 @@ def Generate_RandomPositionRoughFine(generations_budget,Nb_Points_Generation,Nb_
     Pos_rough = np.stack([X.ravel(), Y.ravel()], axis=-1)
 
     i=0
+    print('Carefull: not hull method.')
     for j in range(generations_budget):
         k=0
         while k<Nb_Points_Generation:
@@ -84,7 +127,7 @@ def Generate_RandomPositionRoughFine(generations_budget,Nb_Points_Generation,Nb_
             Pos = PosFiltered
             Pos_df['x_piezo']=Pos[:,0]
             Pos_df['y_piezo']=Pos[:,1]
-            Pos_df['z_piezo']=Find_zFromEq(Pos_rough[i,0]+0.001*Pos[:,0],Pos_rough[i,1]+0.001*Pos[:,1],Coeff)
+            Pos_df['z_piezo']=estimate_z(Pos_rough[i,0]+0.001*Pos[:,0],Pos_rough[i,1]+0.001*Pos[:,1],PointsCalib,Coeff[0],Coeff[1],Coeff[2])
             Pos_df['x_rough']=Pos_rough[i,0]
             Pos_df['y_rough']=Pos_rough[i,1]
             Pos_df['Cluster']=i
@@ -110,8 +153,6 @@ def LoadDataFromFiles(FileDir, FolderCalibWavelength, WaveCenter):
         temp = pd.read_csv(FolderCalibWavelength, sep='\t', decimal=',')
         a = temp.loc[:, 'a'].to_numpy()
         b = temp.loc[:, 'b'].to_numpy()
-        print('Reading wavelength calibration at : {}'.format(
-            FolderCalibWavelength))
     except:
         print('Problem reading wavelength calibraton at : {}\n Taking default value.'.format(
             FolderCalibWavelength))
@@ -121,7 +162,7 @@ def LoadDataFromFiles(FileDir, FolderCalibWavelength, WaveCenter):
     CenterPixel = WaveCenter
     Wavelength = (PixelNumber-b)/a+CenterPixel
     
-    File= sorted(glob.glob(FileDir+'/*.spe'),key=lambda x: float(x[x.find('data')+4:x.find('.spe')]))
+    File= sorted(glob.glob(FileDir+'/*[0-9].spe'),key=lambda x: float(x[x.find('data')+4:x.find('.spe')]))
     DataTot = pd.DataFrame(columns=list(Wavelength)+['Time','Mes'])
 
     for j in range(len(File)):
@@ -132,7 +173,8 @@ def LoadDataFromFiles(FileDir, FolderCalibWavelength, WaveCenter):
         TimeI = MetaData.loc[:, 0].to_numpy()/(1E6)  # Time in ms
         DataTotTemp=pd.DataFrame(columns=list(Wavelength)+['Time','Mes'])
         try:
-            DataTotTemp[Wavelength]=np.squeeze(DataRaw.data)
+            temp=np.squeeze(DataRaw.data)
+            DataTotTemp[Wavelength]=np.squeeze(temp)            
         except Exception as e:
             print('\n{}'.format(e))
             exit
@@ -150,9 +192,9 @@ def LoadDataFromFiles(FileDir, FolderCalibWavelength, WaveCenter):
 #############################
 
 def DoFocusAcqSeries(camera,PiezoAxis):
-    Piezo_zPos=np.linspace(start=0.1,stop=79.9,num=100)
+    Piezo_zPos=np.linspace(start=15,stop=70,num=100)
     NbFrame=camera.GetNumberOfFrame()
-    camera.SetNumberOfFrame(3.)
+    #camera.SetNumberOfFrame(2.)
     for pos in Piezo_zPos:
         print('\r Focus finding :{} %'.format(np.round(100*pos/Piezo_zPos[-1], 1)), end='', flush=True)
         PiezoAxis.MoveTo(pos)
@@ -193,7 +235,7 @@ def best_fit_plane(x, y, z):
     coefficients, _, _, _ = np.linalg.lstsq(X, z,rcond=None)
     return coefficients  # A, B, C
 
-def FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosRoughAxis):
+def FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosRoughAxis,Ptsindex):
     ''''This function take as argument will scan to z-axis and mesure the intensity then fit the curve find the maximum position and return it in a single float. The unit is in um.
     The argument are the following:
 
@@ -208,7 +250,7 @@ def FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosR
     -PiezoAxis: a PiezoAxisControl from ControlPiezo
 
     -PosRoughAxis: an array containing the [x,y] position of the rough axis this is only used for saving the data file'''
-    DataFolderRaw=DataFolder+'/RawDataFocusFind/Datax={}y={}'.format(np.round(PosRoughAxis[0],3),np.round(PosRoughAxis[1],3))
+    DataFolderRaw=DataFolder+'/RawDataFocusFind/{}.Datax={}y={}'.format(Ptsindex,np.round(PosRoughAxis[0],3),np.round(PosRoughAxis[1],3))
     os.makedirs(DataFolderRaw, exist_ok=True)
 
     # Acquisition
@@ -217,8 +259,8 @@ def FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosR
 
     # Load data
     Data=LoadDataFromFiles(DataFolderRaw,WaveCalibFolder,WavelengthSpectro)
-    Data.to_pickle(DataFolder+"/RawDataFocusFind/Datax={}y={}.pkl".format(np.round(PosRoughAxis[0],3),np.round(PosRoughAxis[1],3)))
-    pd.DataFrame(zPos,columns=['z']).to_csv(DataFolder+"/RawDataFocusFind/Zpos_x={}y={}.csv".format(np.round(PosRoughAxis[0],3),np.round(PosRoughAxis[1],3)))
+    Data.to_pickle(DataFolder+"/RawDataFocusFind/{}-Datax={}y={}.pkl".format(Ptsindex,np.round(PosRoughAxis[0],3),np.round(PosRoughAxis[1],3)))
+    pd.DataFrame(zPos,columns=['z']).to_csv(DataFolder+"/RawDataFocusFind/{}-Zpos_x={}y={}.csv".format(Ptsindex,np.round(PosRoughAxis[0],3),np.round(PosRoughAxis[1],3)))
     # Here the focus is assumed just to be the point in which there is a maximum. With a fit 
     # it sometimes doesnt fit well because of a reflection which create a sholder in the peak.
     Mes_posz=Data.loc[:,'Mes'].unique()
@@ -226,18 +268,25 @@ def FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosR
 
     for ztemp in Mes_posz:
         DataTemp=Data.loc[ Data['Mes']==ztemp,:].iloc[:,:-2]
-        temp=pd.DataFrame({'z':zPos[int(ztemp)],'Mean of max':DataTemp.max(axis=1).mean(),'std':DataTemp.max(axis=1).std()},index=[0])
-        DataAgg=pd.concat([DataAgg,temp],axis=0,ignore_index=True)
+        try:
+            temp=pd.DataFrame({'z':zPos[int(ztemp)],'Mean of max':DataTemp.max(axis=1).mean(),'std':DataTemp.mean(axis=0).std()},index=[0])
+        except:
+            print(Mes_posz)
+            exit
+        if DataAgg.shape[0]==0:
+            DataAgg=temp
+        else:
+            DataAgg=pd.concat([DataAgg,temp],axis=0,ignore_index=True)
     DataAgg=DataAgg.dropna()
-    p_opt,p_cov=curve_fit(intensity_z,xdata=DataAgg.loc[:,'z'],ydata=DataAgg.loc[:,'Mean of max'])
+    #p_opt,p_cov=curve_fit(intensity_z,xdata=DataAgg.loc[:,'z'],ydata=DataAgg.loc[:,'Mean of max'])
     
-    z_fit=np.linspace(start=0.1,stop=79.9,num=500)
-    Data_fit=intensity_z(z_fit,p_opt[0],p_opt[1],p_opt[2],p_opt[3])
+    #z_fit=np.linspace(start=0.1,stop=79.9,num=500)
+    #Data_fit=intensity_z(z_fit,p_opt[0],p_opt[1],p_opt[2],p_opt[3])
     FocusPos=DataAgg.loc[DataAgg['Mean of max'].idxmax(axis=0),'z']
 
     fig,ax=plt.subplots(1,1)
     ax.errorbar(DataAgg['z'],DataAgg['Mean of max'],yerr=DataAgg['std'],label=str(np.round(FocusPos,3)))
-    ax.plot(z_fit,Data_fit)
+    #ax.plot(z_fit,Data_fit)
     ax.plot([FocusPos,FocusPos],ax.get_ylim())
     ax.set_xlabel('z[$\mu$m]')
     plt.legend()
@@ -246,7 +295,7 @@ def FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosR
 
     return FocusPos
 
-def FindPlaneEquation(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosRoughAxis,x_rough,y_rough):
+def FindPlaneEquation(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosRoughAxis,x_rough,y_rough,Laser):
     ''''This function will find the plane equation that can be used to correct for sample tilt.
     The argument are the following:
 
@@ -274,11 +323,16 @@ def FindPlaneEquation(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoA
     for i in range(Nb_points):
         x_rough.MoveTo(PosRoughAxis[i,0])
         y_rough.MoveTo(PosRoughAxis[i,1])
-        Points.loc[i,'x[mm]']=PosRoughAxis[i,0]
-        Points.loc[i,'y[mm]']=PosRoughAxis[i,1]
-        Points.loc[i,'z[um]']=FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosRoughAxis[i,:])
-        print(Points)
+        Points.loc[i,'x[mm]']=x_rough.GetPosition() #PosRoughAxis[i,0]
+        Points.loc[i,'y[mm]']=y_rough.GetPosition() #PosRoughAxis[i,1]
+        Laser.SetStatusShutterTunable(1)
+        Points.loc[i,'z[um]']=FindFocus(DataFolder,WaveCalibFolder,WavelengthSpectro,Camera,PiezoAxis,PosRoughAxis[i,:],i)
+        Laser.SetStatusShutterTunable(0)
+        print()
+        print('Points {}/{}'.format(i,Nb_points))
+    print(Points)
     Coeff=best_fit_plane(Points.loc[:,'x[mm]'].astype('float'),Points.loc[:,'y[mm]'].astype('float'),Points.loc[:,'z[um]'].astype('float'))
+    Points.to_csv(DataFolder+'/SaveActualPoints.csv')
     return Coeff
 
 
@@ -294,8 +348,8 @@ if __name__ == '__main__':
     
     FolderCalibWavelength = '//sun/garnett/home-folder/gautier/Femto-setup/Data/0.Calibration/Spectrometer.csv'
 
-    Spectrograph_slit=100
-    Spectrograph_Center=700
+    Spectrograph_slit=200
+    Spectrograph_Center=800
 
     GeneralPara = {'Experiment name': ' FocusFind'}
 
@@ -320,7 +374,12 @@ if __name__ == '__main__':
 
     InstrumentsPara['Kymera'] = spectro.parameterDict
     print('Initialised spectro')
-    
+    #############################
+    # Initialisation of pulse picker
+    #############################
+    pp = picker.PulsePicker("USB0::0x0403::0xC434::S09748-10A7::INSTR")
+    InstrumentsPara['Pulse picker'] = pp.parameterDict
+    print('Initialised pulse picker')
     #############################
     # Initialisation of piezo
     #############################
@@ -333,8 +392,8 @@ if __name__ == '__main__':
     y_axis.MoveTo(0.05)
     InstrumentsPara['PiezoStage']=piezo.parameterDict
 
-    x_axis_Rough = RoughTransla.ConexController('COM12')
-    y_axis_Rough = RoughTransla.ConexController('COM13')
+    x_axis_Rough = RoughTransla.ConexController('COM12')# Normal COM12
+    y_axis_Rough = RoughTransla.ConexController('COM14')# Normal COM14
     InstrumentsPara['Rough Stage']=x_axis_Rough.parameterDict | y_axis_Rough.parameterDict 
     print('Initialised rough translation stage')
 
@@ -354,14 +413,18 @@ if __name__ == '__main__':
     
     InstrumentsPara['PI EMCCD'] = camera.parameterDict
     print('Initialised EMCCD')
-    Laser.SetStatusShutterTunable(1)
-    x_begin=9.6
-    y_begin=10
-    x=[x_begin,x_begin+1,x_begin,x_begin+1,x_begin-1,x_begin-1]
-    y=[y_begin,y_begin,y_begin+1,y_begin+1,y_begin,y_begin-1]
-    Pos=np.transpose(np.array([x,y]))
-    Laser.SetStatusShutterTunable(0)
+    
+    x_begin=0
+    y_begin=6
+    
+    x=np.linspace(x_begin,x_begin+2.5,5)
+    y=np.linspace(y_begin,y_begin+2.5,6)
+    X, Y = np.meshgrid(x, y)
+    Pos=np.stack([X.ravel(), Y.ravel()], axis=-1)
+    index = random.sample(range(0, Pos.shape[0]), Pos.shape[0]) #comment those line if you want the points to be unshuffled 
+    Pos = Pos[index, :]
+    
+    Coeff=FindPlaneEquation(DirectoryPath,FolderCalibWavelength,spectro.GetWavelength(),camera,z_axis,Pos,x_axis_Rough,y_axis_Rough,Laser)
 
-    Coeff=FindPlaneEquation(DirectoryPath,FolderCalibWavelength,spectro.GetWavelength(),camera,z_axis,Pos,x_axis_Rough,y_axis_Rough)
-    print(Coeff)
+    Laser.SetStatusShutterTunable(0)
     
